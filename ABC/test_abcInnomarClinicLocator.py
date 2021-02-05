130587f19982a6aec3ad7e29130e377af194838d
Steven Blahut <sblahut@geekhive.com>
2021-02-05 10:53:32 -0500

    added region, zip, current location test cases

--- ABC/test_abcInnomarClinicLocator.py
+++ ABC/test_abcInnomarClinicLocator.py
@@ -41,23 +41,98 @@ def test_abcInnomarClinicLocator ():
     search = driver.find_element(By.CSS_SELECTOR, "body > div:nth-child(2) > main > div > div.content__middle > div.location-finder > div.location-finder-search-wrapper.background-true-blue > div > div > div > form > div > label > input")
     
     search.send_keys("Seattle")
+    print('....Searching for Seattle....')
+    print("                                          ")
+    driver.find_element(By.CSS_SELECTOR, "body > div:nth-child(2) > main > div > div.content__middle > div.location-finder > div.location-finder-search-wrapper.background-true-blue > div > div > div > form > div > button.location-finder-search__button").click()
 
-    search.click()
+    # Wait
+    time.sleep(2)
 
     # Run Results
     ids = driver.find_elements(By.CLASS_NAME, "location-card__address")
     
     print("                                          ")
     print("##########################################")
-    print("########## First Result: Seattle #########")
+    print("########## Test Case #1: Seattle #########")
     for id in islice(ids, 0, 3, 1):
         
-        print("Adress: " + str(id.text))
+        print("Address: " + str(id.text))
     print("##########################################")
     print("                                          ")
 
-    
+    # Wait
+    time.sleep(2)
 
+    # Search 'Central'
+    search.clear()
+    search.send_keys("Central")
+    print('....Searching for Central Region....')
+    print("                                          ")
+    driver.find_element(By.CSS_SELECTOR, "body > div:nth-child(2) > main > div > div.content__middle > div.location-finder > div.location-finder-search-wrapper.background-true-blue > div > div > div > form > div > button.location-finder-search__button").click()
+
+    # Wait
+    time.sleep(2)
+
+    # Run Results
+    ids = driver.find_elements(By.CLASS_NAME, "location-card__address")
+    
+    print("                                          ")
+    print("##########################################")
+    print("########## Test Case #2: Central #########")
+    for id in islice(ids, 0, 3, 1):
+        
+        print("Address: " + str(id.text))
+    print("##########################################")
+    print("                                          ")
+    
+    # Wait
+    time.sleep(2)
+
+    # Search '98101'
+    search.clear()
+    search.send_keys("98101")
+    print('....Searching for 98101 Zip Code....')
+    print("                                          ")
+    driver.find_element(By.CSS_SELECTOR, "body > div:nth-child(2) > main > div > div.content__middle > div.location-finder > div.location-finder-search-wrapper.background-true-blue > div > div > div > form > div > button.location-finder-search__button").click()
+
+    # Wait
+    time.sleep(2)
+
+    # Run Results
+    ids = driver.find_elements(By.CLASS_NAME, "location-card__address")
+    
+    print("                                          ")
+    print("##########################################")
+    print("########## Test Case #3: 98101 ##########")
+    for id in islice(ids, 0, 3, 1):
+        
+        print("Address: " + str(id.text))
+    print("##########################################")
+    print("                                          ")
+
+    # Wait
+    time.sleep(2)
+
+    # Search 'Current Location'
+    search.clear()
+    print('....Searching by Current Location....')
+    print("                                          ")
+    driver.find_element(By.CSS_SELECTOR, "body > div:nth-child(2) > main > div > div.content__middle > div.location-finder > div.location-finder-search-wrapper.background-true-blue > div > div > div > form > div > button.location-finder-search__button--location").click()
+
+    # Wait
+    time.sleep(2)
+
+    # Run Results
+    ids = driver.find_elements(By.CLASS_NAME, "location-card__address")
+    
+    print("                                          ")
+    print("##########################################")
+    print("##### Test Case #4: Current Location #####")
+    for id in islice(ids, 0, 3, 1):
+        
+        print("Address: " + str(id.text))
+    print("##########################################")
+    print("                                          ")
 
 # Call Method
 test_abcInnomarClinicLocator ()
